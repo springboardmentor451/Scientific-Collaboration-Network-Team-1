f36c3980c9f48b822ac08ac29d3b28766d31b826
RishabhSinghal04 <rishabhrs2302@gmail.com>
2026-08-23 13:40:52 +0530

    replaced "code" with "password"

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -5,9 +5,9 @@ from pydantic import SecretStr
 pwd_hasher = PasswordHash([Argon2Hasher()])
 
 
-def hash_code(code: SecretStr) -> str:
-    return pwd_hasher.hash(code.get_secret_value())
+def hash_password(password: SecretStr) -> str:
+    return pwd_hasher.hash(password.get_secret_value())
 
 
-def verify_code(plain: SecretStr, hashed: str) -> bool:
+def verify_password(plain: SecretStr, hashed: str) -> bool:
     return pwd_hasher.verify(plain.get_secret_value(), hashed)
